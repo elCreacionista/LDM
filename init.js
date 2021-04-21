226183e054d47b4b24ba92100b9613d9c2f53c3c
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-04-21 13:54:54 +0200

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-if (localStorage.nocturn_mode == null){
-    localStorage.nocturn_mode = true;
-}
-if (localstorage_boolean_traductor('nocturn_mode')){
-    night.innerHTML = '<link rel="stylesheet" href="../CSS/style_nocturn.css">';
-}
-else{
-    night.innerHTML = '<link rel="stylesheet" href="../CSS/style_daily.css">';
-}
