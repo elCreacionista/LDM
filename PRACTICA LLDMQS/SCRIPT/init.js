717a261a5ddf9b23595aece63daed6e1b09f1843
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-05-14 20:23:11 +0200

    ara surten nuvols

--- PRACTICA LLDMQS/SCRIPT/init.js
+++ PRACTICA LLDMQS/SCRIPT/init.js
@@ -7,3 +7,25 @@ if (localstorage_boolean_traductor('nocturn_mode')){
 else{
     night.innerHTML = '<link rel="stylesheet" href="../CSS/style_daily.css">';
 }
+clouds.push(CreateTree(document.querySelector("main"), true, 0));
+clouds.push(CreateTree(document.querySelector("main"), true, 1));
+clouds.push(CreateTree(document.querySelector("main"), true, 2));
+clouds.push(CreateTree(document.querySelector("main"), true, 3));
+console.log(clouds);
+const moveClouds = setInterval(movesky, 1);
+
+function movesky(){
+    for(let i = 0; i < clouds.length; i++){
+        /*if (Math.random() < 0.001){
+            clouds.push(CreateTree(document.querySelector("main"), false, i));
+            break;
+        }*/
+        if (clouds[i].p > 1300){
+            document.querySelector("main").removeChild(clouds[i].c);
+            clouds.splice(i ,1, CreateTree(document.querySelector("main"), false, i));
+        }
+        clouds[i].c.style.left = clouds[i].p + "px";
+        clouds[i].p ++;
+    }
+
+}
