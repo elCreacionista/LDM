3455420ad1d2f526ca7fa98783879296fe43d273
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-05-14 20:22:52 +0200

    Ara son mes nuvuloses

--- PRACTICA LLDMQS/SCRIPT/svgses.js
+++ PRACTICA LLDMQS/SCRIPT/svgses.js
@@ -1,12 +1,29 @@
-function CreateTree(DOMElement){
+function CreateTree(DOMElement, position, id){
     const W3 = "http://www.w3.org/2000/svg";
     let svg = document.createElementNS(W3, 'svg');
-    for (let i = 0; i < 20; i++){
-        let leave = new Leave(20, 60, 20, 20, 10, 10);
+    
+    let rand = (Math.random()*1000);
+    const x = position  ? rand : -100;
+    
+    svg.classList.add("cloud");
+    svg.id = "cloud" + clouds.length;
+    svg.style.position = "absolute";
+    svg.style.top = (Math.random()*20) + "px";
+    svg.style.left = x + "px";
+    //svg.style.left =  rand + "px";
+  
+    
+    const item = { c : svg, p : x};
+
+
+
+
+    for (let i = 0; i < 50; i++){
+        let leave = new Leave(20, 150, 10, 40, 4, 10);
         svg.appendChild(Circle(leave));
     }
 
-    DOMElement.appendChild(svg);
+    DOMElement.appendChild(item.c);
 
 
 
@@ -22,9 +39,9 @@ function CreateTree(DOMElement){
         circle.setAttributeNS(null, "cx", obj.px);
         circle.setAttributeNS(null, "cy", obj.py);
         circle.setAttributeNS(null, "r", obj.r);
-        circle.setAttributeNS(null, "fill", "rgb(50," +(200 + Math.random()*50) + ", 50)");
+        let rand = 230 + Math.random()*25
+        circle.setAttributeNS(null, "fill", `rgba(${rand}, ${rand}, ${rand}, ${Math.random() * .5})`);
         return circle;
     }
-
+ return item;
 }
-
