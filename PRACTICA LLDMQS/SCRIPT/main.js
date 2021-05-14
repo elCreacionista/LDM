07d797cbb08cf7576e1eed2cf479cb23aee51361
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-05-14 20:23:36 +0200

    Les nuves

--- PRACTICA LLDMQS/SCRIPT/main.js
+++ PRACTICA LLDMQS/SCRIPT/main.js
@@ -1,12 +1,15 @@
 const configuracio = document.querySelector('#configuracio');
-const nightmode = document.querySelector('#night');
+const night = document.querySelector('#night');
 const config = [];
+console.log(localStorage);
+const clouds = [];
 
-console.log(localStorage)
+document.querySelector("head").innerHTML += '<script src="../SCRIPT/svgeses.js"></script>';
+document.querySelector("head").innerHTML += '<script src="../SCRIPT/init.js"></script>';
+
 
 Header();
 Configurar();
-
 
 function Configurar(){
     let str = ""
@@ -23,26 +26,23 @@ function Configurar(){
 
 
 
-
-
-
 function Header(){
     let string = `
     <div id='title'>
-    <h1>PRÀCTICA <strong id='title_1'>L</strong><strong id='title_2'>L</strong><strong id='title_3'>D</strong><strong id='title_4'>M</strong><strong id='title_5'>Q</strong><h1>
+        <h1>PRÀCTICA <strong id='title_1'>L</strong><strong id='title_2'>L</strong><strong id='title_3'>D</strong><strong id='title_4'>M</strong><strong id='title_5'>Q</strong><h1>
         <div id='nocturn_mode'>
             <div id='nocturn_slider'></div>
             <button id='nocturn_button' onclick="change_nocturn_mode()"></button>
         </div>
-</div>
-<nav id='menu'>
+    </div>
+    <nav id='menu'>
         <a id='a_index' class='menu_unselected' href="index.html">Inici</a>
         <a id='a_jocs' class='menu_unselected' href="jocs.html">Jocs</a>
         <a id='a_ajuda' class='menu_unselected' href="ajuda.html">Ajuda</a>
         <a id='a_mapa' class='menu_unselected' href="mapa.html">Mapa</a>
         <a id='a_config' class='menu_unselected' href="config.html">Configuració</a>
         <a id='a_contact' class='menu_unselected' href="contact.html">Contacte</a>
-</nav>`;
+    </nav>`;
     document.querySelector('#header').innerHTML = string;
     put_actual('index');
     put_actual('jocs');
